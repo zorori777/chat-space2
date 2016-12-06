f05adcbfd5c48053455a942ea1b7cd5c5cf181e9
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2016-12-06 16:37:00 +0900

    association　の定義
    # what
    　モデル間のassociationの定義
    # why
    　モデルをまたいでデータを取り出す
    　コードをわかりやすくするため。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+
+has_many :users, through: :group_users
+has_many :group_users
+has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+has_many   :groups, through: :group_users
+has_many   :groups_user
+has_many   :messages
+
 end
