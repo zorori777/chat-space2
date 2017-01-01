e9023c5bf45c2606d7094557f1e604fc8fdc4cc4
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-01-01 17:07:15 +0900

    追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
 
 def index
-
 end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
         g.test_framework false
         g.helper false
