7d0a59f0ded2392300aacf6b80be9126a1d2b9bb
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-12 14:27:48 +0900

    薬師寺さん指摘改善
    # what
    ・routingの変更
    ・variable ファイルで色の変数定義
    # why

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: %i(new create edit update) do
     scope module: 'groups' do
-      resources :messages, only: [:index]
+      resources :messages, only: %i( index )
     end
   end
-  resources :users, only: [:edit, :update]
+  resources :users, only: %i( edit update)
 end
