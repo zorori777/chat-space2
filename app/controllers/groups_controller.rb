07a6a2b70c490cf503957422af12e40453dc4e7b
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-13 19:42:42 +0900

    user追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
   private
 
   def create_params
-    params.require(:group).permit(:name, user_ids:[])
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def setting_group
