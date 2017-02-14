32d59f48b0aad4acc6e4c9cfba5f79792d3bab53
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-14 21:32:22 +0900

    message routing and controller
    # what
    送信機能
    # why
    メッセージ送信するため

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -1,11 +1,34 @@
 class Groups::MessagesController < GroupsController
 
   before_action :setting_group, only: [:index]
+  before_action :setting_message, only: [:index]
 
   def index
+    # @groups = current_user.groups
+    @group_id = params[:group_id]
+    @messages = Message.find_by(@group_id)
+  end
+
+  def create
+    @message = Message.new(create_message)
+      if @message.save
+         redirect_to group_messages_path, notice: "作成成功しました"
+      else
+         flash.now[:alert] = "作成を失敗しました"
+         render :index
+      end
   end
 
   private
+
+  def setting_message
+    @message = Message.new
+  end
+
+  def create_message
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 
   def setting_group
    @groups = current_user.groups
