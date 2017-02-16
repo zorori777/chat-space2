fe983cafcb118722e96f602a3d176832c697f440
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-16 18:14:43 +0900

    controller , validation
    # WHAT
    message機能
    # WHY
    message機能

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -3,32 +3,38 @@ class Groups::MessagesController < GroupsController
   before_action :setting_group, only: [:index]
   before_action :setting_message, only: [:index]
 
+
   def index
     # @groups = current_user.groups
-    @group_id = params[:group_id]
-    @messages = Message.find_by(@group_id)
+    # @group = Group(params[:group_id])
+    @message = Message.new
+  end
+
+  def new
   end
 
   def create
-    @message = Message.new(create_message)
+    @message = current_user.messages.new(create_message)
       if @message.save
          redirect_to group_messages_path, notice: "作成成功しました"
       else
+        @groups = current_user.groups
+        @group = Group.find(params[:group_id])
+        @messages = @group.messages
          flash.now[:alert] = "作成を失敗しました"
-         render :index
+         render action: :index
       end
   end
 
   private
 
-  def setting_message
-    @message = Message.new
-  end
-
   def create_message
-    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
 
+  def setting_message
+    @messages = @group.messages.includes(:user)
+  end
 
   def setting_group
    @groups = current_user.groups
