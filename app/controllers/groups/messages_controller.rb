d3891c83dbae1b35ee67ec5950d22a451cf1efc9
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-18 12:57:48 +0900

    view　controller  modify
    #what
    viewの調整
    #why
    message機能の実装

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -1,12 +1,10 @@
 class Groups::MessagesController < GroupsController
 
-  before_action :setting_group, only: [:index]
-  before_action :setting_message, only: [:index]
+  before_action :setting_group, only: [:index, :create]
+  before_action :setting_message, only: [:index, :create]
 
 
   def index
-    # @groups = current_user.groups
-    # @group = Group(params[:group_id])
     @message = Message.new
   end
 
@@ -16,13 +14,10 @@ class Groups::MessagesController < GroupsController
   def create
     @message = current_user.messages.new(create_message)
       if @message.save
-         redirect_to group_messages_path, notice: "作成成功しました"
+         redirect_to group_messages_path(@group), notice: "作成成功しました"
       else
-        @groups = current_user.groups
-        @group = Group.find(params[:group_id])
-        @messages = @group.messages
          flash.now[:alert] = "作成を失敗しました"
-         render action: :index
+         render :index
       end
   end
 
