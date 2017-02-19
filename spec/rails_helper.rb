6724deaba3f6f139974cac1d72bfd3dd1ee46ded
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-19 21:01:27 +0900

    Gem周り　設定変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,9 +10,9 @@ RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include FactoryGirl::Syntax::Methods
-
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
+
+  config.include FactoryGirl::Syntax::Methods
 end
