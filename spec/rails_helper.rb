97e772ddf5b72ce47c45daab7f529642cace25af
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-25 21:39:13 +0900

    rails_helper.rb modify

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,8 +18,8 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
 
-  config.include ControllerMacros, :type => :controller
+  config.include ControllerMacros, type: :controller
 end
 
