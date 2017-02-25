7365bf604029932c2a4c9bfcddb77c32950471f4
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-25 21:20:56 +0900

    message_controller test renew

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'shoulda/matchers' #
+require 'devise'
+require 'support/controller_macros'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -16,18 +17,9 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.include ControllerMacros, :type => :controller
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # 今回はRspecを使うのでこのように設定
-    with.test_framework :rspec
-
-    # shoulda-matchersを使いたいテストライブラリを指定
-    with.library :active_record
-    with.library :active_model
-    with.library :action_controller
-    # Or, choose the following (which implies all of the above):
-    with.library :rails
-  end
-end
