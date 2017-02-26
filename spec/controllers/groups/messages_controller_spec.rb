8598c0a7660ae08038ac7ec707a923ed34ea0751
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-26 09:50:51 +0900

    message_test_controller modify

--- spec/controllers/groups/messages_controller_spec.rb
+++ spec/controllers/groups/messages_controller_spec.rb
@@ -1,54 +1,69 @@
 require 'rails_helper'
- require 'devise'
+require 'devise'
 
- describe Groups::MessagesController do
+describe Groups::MessagesController do
 
-   let(:user){ create(:user) }
-   let(:group){ create(:group) }
-   let(:message){ create(:message, user_id: user.id, group_id: group.id) }
-   let(:messages){ create_list(:message,3, user: user, group: group) }
+ let(:user){ create(:user) }
+ let(:group){ create(:group) }
+ let(:message){ create(:message, user_id: user.id, group_id: group.id) }
+ let(:messages){ create_list(:message,3, user: user, group: group) }
 
-   describe 'GET #index' do
+ describe 'GET #index' do
+
+  context "login user " do
 
    before do
      login_user user
      get :index, group_id: group
    end
 
-     it "populates an array of messages ordered by created_at DESC" do
-     expect(assigns(:messages)).to match(messages)
-     end
+    it "assigns the requested message to @message" do
+      message = build(:message)
+      expect(assigns(:message)).to be_a_new(Message)
+    end
 
-     it "assigns the requesteed message to @group" do
-       expect(assigns(:group)).to eq group
-     end
+    it "populates an array of messages ordered by created_at DESC" do
+      expect(assigns(:messages)).to match(messages)
+    end
 
-     it "renders the :index template" do
-       expect(response).to render_template :index
-     end
+    it "assigns the requesteed message to @group" do
+      expect(assigns(:group)).to eq group
+    end
+
+    it "renders the :index template" do
+      expect(response).to render_template :index
+    end
+  end
+
+  context "logout user" do
+
+    it "is not sigh in in the chat-space" do
+      get :index, params: { group_id: group.id, user_id: user.id }
+      expect(response).to redirect_to  new_user_session_path
+    end
+  end
  end
 
-   describe 'POST #create' do
+ describe 'POST #create' do
 
    before do
      login_user user
    end
 
-     it "saves the new message in the database" do
+    it "saves the new message in the database" do
       expect{
-       post :create,group_id:group.id, message: attributes_for(:message)
-       }.to change(Message, :count).by(1)
-     end
+      post :create,group_id:group.id, message: attributes_for(:message)
+      }.to change(Message, :count).by(1)
+    end
 
-     it "redirects to messages #index" do
-       post :create, group_id: group.id, message: attributes_for(:message)
-       expect(response).to redirect_to group_messages_path
-     end
+    it "redirects to messages #index" do
+      post :create, group_id: group.id, message: attributes_for(:message)
+      expect(response).to redirect_to group_messages_path
+    end
 
-     it "not save messaege in the database and render index_" do
-       post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group.id)
-       expect(response).to render_template(:index)
-     end
-
-   end
- end
+    it "not save messaege in the database and render index_" do
+      post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group.id)
+      expect(response).to render_template(:index)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,5 +21,6 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 
   config.include ControllerMacros, type: :controller
+
 end
 
