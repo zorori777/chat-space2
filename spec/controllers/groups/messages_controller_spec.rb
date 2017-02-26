321152bf2e4a14001f819ccc0afcb84af6c7f82e
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-02-26 14:04:41 +0900

    message_controller_test 4

--- spec/controllers/groups/messages_controller_spec.rb
+++ spec/controllers/groups/messages_controller_spec.rb
@@ -8,33 +8,7 @@ describe Groups::MessagesController do
  let(:message){ create(:message, user_id: user.id, group_id: group.id) }
  let(:messages){ create_list(:message,3, user: user, group: group) }
 
- describe 'GET #index' do
-
-  context "login user " do
-
-   before do
-     login_user user
-     get :index, group_id: group
-   end
-
-    it "assigns the requested message to @message" do
-      expect(assigns(:message)).to be_a_new(Message)
-    end
-
-    it "populates an array of messages ordered by created_at DESC" do
-      expect(assigns(:messages)).to match(messages)
-    end
-
-    it "assigns the requesteed message to @group" do
-      expect(assigns(:group)).to eq group
-    end
-
-    it "renders the :index template" do
-      expect(response).to render_template :index
-    end
-  end
-
-  context "logout user" do
+  describe "logout user" do
 
     it "reditects to sign_up_path" do
       get :index, params: { group_id: group, user_id: user }
@@ -42,39 +16,64 @@ describe Groups::MessagesController do
     end
   end
 
- end
+  describe 'login user' do
 
- describe 'POST #create' do
-
-   before do
+  before do
      login_user user
+  end
+
+   context 'GET #index' do
+
+    before do
+     get :index, group_id: group
+    end
+
+      it "assigns the requested message to @message" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it "populates an array of messages ordered by created_at DESC" do
+        expect(assigns(:messages)).to match(messages)
+      end
+
+      it "assigns the requesteed message to @group" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
    end
 
-    it "saves the new message in the database" do
-      expect{
-      post :create, group_id:group, message: attributes_for(:message)
-      }.to change(Message, :count).by(1)
-    end
 
-    it "flashes a notice save_message" do
-      post :create, group_id: group, message: attributes_for(:message)
-      expect(flash[:notice]).to include ("作成成功しました")
-    end
+   context 'POST #create' do
 
-    it "redirects to messages #index" do
-      post :create, group_id: group, message: attributes_for(:message)
-      expect(response).to redirect_to group_messages_path
-    end
+      it "saves the new message in the database" do
+        expect{
+        post :create, group_id:group, message: attributes_for(:message)
+        }.to change(Message, :count).by(1)
+      end
 
-    it "flashes a alert unsave_message" do
-      post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group)
-      expect(flash[:alert] ). to include ("作成を失敗しました")
-    end
+      it "flashes a notice save_message" do
+        post :create, group_id: group, message: attributes_for(:message)
+        expect(flash[:notice]).to include ("作成成功しました")
+      end
 
-    it "not save messaege in the database and render index_" do
-      post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group)
-      expect(response).to render_template(:index)
-    end
+      it "redirects to messages #index" do
+        post :create, group_id: group, message: attributes_for(:message)
+        expect(response).to redirect_to group_messages_path
+      end
 
- end
+      it "flashes a alert unsave_message" do
+        post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group)
+        expect(flash[:alert] ). to include ("作成を失敗しました")
+      end
+
+      it "not save messaege in the database and render index_" do
+        post :create, group_id: group, message: attributes_for(:message,body: "", group_id: group)
+        expect(response).to render_template(:index)
+      end
+   end
+
+  end
 end
