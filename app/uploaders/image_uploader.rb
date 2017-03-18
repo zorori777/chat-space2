7288d792d005ce00c69d8c1fe7a3a00bd084fcad
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-03-18 19:21:50 +0900

    final modify

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-   if Rails.env.test?
-      storage :file
-    else
-      storage :fog
-  end
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
