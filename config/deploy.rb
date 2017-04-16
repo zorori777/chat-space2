7bd03ebccbbb3eda5d46c0ccb966ceca14b9236d
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-04-16 20:06:45 +0900

    Fix for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid only for current version of Capistrano
+lock ' 3.8.0 '
+
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:chat-space2.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chat-space.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
